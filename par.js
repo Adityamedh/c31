442fa8a21b1bdb032a55df53aff501a1504219d8
Aj gamer <65167907+Adityamedh@users.noreply.github.com>
2020-07-24 22:20:07 +0530

    Add files via upload

---
+++ par.js
@@ -0,0 +1,29 @@
+class Par{
+constructor(x,y,r){
+var options={
+
+restitution: 0.4
+
+}
+this.r=r;
+this.body = Bodies.circle(x,y,yhis.r,options)
+this.color=color(random(0,255),(random(0,255),(random(0,255))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
